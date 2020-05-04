ec5f10c8a7ddaffa669decf478263f386355af9c
prade091078 <prade.accet@gmail.com>
2020-05-05 03:52:10 +0530

    Initial commit

---
+++ app/src/main/java/com/prade/splitappdemo/MainActivity.java
@@ -0,0 +1,29 @@
+package com.prade.splitappdemo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.res.Configuration;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        if (isInMultiWindowMode()) {
+            //we are in multiwindow
+        }
+
+        if (isInPictureInPictureMode()) {
+
+        }
+    }
+
+    @Override
+    public void onMultiWindowModeChanged(boolean isInMultiWindowMode, Configuration newConfig) {
+        super.onMultiWindowModeChanged(isInMultiWindowMode, newConfig);
+    }
+}
+
